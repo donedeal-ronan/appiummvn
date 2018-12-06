8a1468fb580107eb37e96b043868746e46a3e695
Ronan <ronan@donedeal.ie>
2018-12-06 15:36:51 +0000

    - Added some temporary logout logic. - Added a script to run the login - logout loop 10 times for Jenkins stress testing.

--- src/test/java/POM/generic/LoginPageObject.java
+++ src/test/java/POM/generic/LoginPageObject.java
@@ -15,27 +15,52 @@ public class LoginPageObject implements LoginPage {
 
     @AndroidFindBy(id = "mEmailInputET")
     @iOSFindBy(accessibility = "email")
-    MobileElement emailInput;
+    private MobileElement emailInput;
 
     @AndroidFindBy(id = "mPasswordInputET")
     @iOSFindBy(accessibility = "password")
-    MobileElement passwordInput;
+    private MobileElement passwordInput;
 
     @AndroidFindBy(id = "login_button")
     @iOSFindBy(accessibility = "LOGIN")
-    MobileElement loginBtn;
+    private MobileElement loginBtn;
+
+    @AndroidFindBy(id = "action_account")
+    @iOSFindBy(accessibility = "My Account")
+    private MobileElement accountTab;
+
+    @AndroidFindBy(id = "my_account_logout_button")
+    @iOSFindBy(accessibility = "LOGOUT")
+    private MobileElement logoutBtn;
 
     public LoginPageObject(AppiumDriver driver) {
         PageFactory.initElements(new AppiumFieldDecorator(driver), this);
         this.driver = driver;
     }
 
-    public void insertEmailAddress() {
-        emailInput.sendKeys(emailAddress);
+    public void login() {
+        insertPassword(); // Password is needed first due to dismissing the keyboard calling the "Done" btn on iOS.
+        insertEmailAddress();
+        driver.hideKeyboard();
+        clickLoginBtn();
+        logout();
+    }
+
+    private boolean isLoggedIn() {
+        return !passwordInput.isDisplayed();
+    }
+
+    private void logout() {
+        accountTab.click();
+        logoutBtn.click();
     }
 
     public void insertPassword() {
         passwordInput.sendKeys(password);
+    }
+
+    public void insertEmailAddress() {
+        emailInput.sendKeys(emailAddress);
     }
 
     public void clickLoginBtn() {
@@ -46,12 +71,5 @@ public class LoginPageObject implements LoginPage {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-    public void login() {
-        insertPassword(); // Password is needed first due to dismissing the keyboard calling the "Done" btn on iOS.
-        insertEmailAddress();
-        driver.hideKeyboard();
-        clickLoginBtn();
     }
 }
